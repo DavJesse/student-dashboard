661446c14bd6bda50162228a28542c7eab979054
davodhiambo <jesseomolo@yahoo.com>
2025-06-14 20:56:21 +0300

    feat: add function to render login form. files: move logo to logo folder for separation of concerns.

---
+++ scripts/views/login.js
@@ -0,0 +1,24 @@
+/**
+ * Renders the login view as an HTML string.
+ * This function generates a login form with username and password fields,
+ * along with a submit button, and includes a logo and header.
+ * 
+ * @returns {string} The HTML string for the login view.
+ */
+export function renderLogin() {
+    return `
+        <div id="login_container">
+        <img src="${window.location.origin}/assets/images/logo.png" alt="Logo" id="logo">
+        <h1 id="login_header">Login Here</h1>
+        <form id="login_form">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+            
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            
+            <button type="submit">Login</button>
+        </form>
+        </div>
+    `;
+}
